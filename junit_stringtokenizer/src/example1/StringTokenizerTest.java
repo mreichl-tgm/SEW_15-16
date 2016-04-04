0bdead55d77c03db65a259fd2217d25ef4db0b9f
Markus Reichl <mreichl@student.tgm.ac.at>
2016-04-04 14:54:23 +0200

    Adding stuff done on Mondey, lel

--- junit_stringtokenizer/src/example1/StringTokenizerTest.java
+++ junit_stringtokenizer/src/example1/StringTokenizerTest.java
@@ -1,5 +1,31 @@
 package example1;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.StringTokenizer;
+
+import java.util.NoSuchElementException;
+
 public class StringTokenizerTest {
+	
+	@Test
+	public void countTokensTest() {
+		StringTokenizer st = new StringTokenizer("Test me if you can!");
+		int count = st.countTokens();
+		System.out.println(count);
+		assertEquals(5, count);
+	}
+	
+	@Test
+	public void nextTokenTest() {
+		StringTokenizer st = new StringTokenizer("");
+		try {
+			st.nextToken();
+		} catch (NoSuchElementException e) {
+			assertEquals("NoSuchElementException", e);
+		}
+	}
 
 }
